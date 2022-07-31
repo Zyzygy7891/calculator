791421e04efba6ee8166d76cf1435437969e4b14
Attila Surányi <suranyiattila87@gmail.com>
2022-07-31 10:42:26 +0200

    Define languange texts

---
+++ js/lang.js
@@ -0,0 +1,30 @@
+/*
+    Defines a global variable for the apps text, error messages, etc
+*/
+
+const CALCULATORLANG = new Map([
+  [
+    "hu",
+    new Map([
+      ["ac", "AC"],
+      ["plusminus", "<sup>+</sup>/<sub>-</sub>"],
+      ["percent","%"],
+      ["divide","/"],
+      ["multiplication","*"],
+      ["minus","-"],
+      ["plus","+"],
+      ["result","="],
+      ["decimal",","],
+      ["9",""],
+      ["8",""],
+      ["7",""],
+      ["6",""],
+      ["5",""],
+      ["4",""],
+      ["3",""],
+      ["2",""],
+      ["1",""],
+      ["0",""]
+    ]),
+  ]
+]);
